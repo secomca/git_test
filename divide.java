4a7c529acd2d941238c136e4a40fdee04486d4e0
secomca <garciaricardojf@gmail.com>
2020-07-01 05:36:27 -0400

    Add divide.java nd divide.class

---
+++ divide.java
@@ -0,0 +1,10 @@
+public class divide{
+    
+    public static void main(String[] args){
+    int x = 10;
+    int y = 10;
+    int result = x / y;
+        System.out.println("result: "+result);     
+    }
+    
+}
